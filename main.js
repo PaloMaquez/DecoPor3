2e4e57ad374f7c79160bac89d5cc78477ad415c0
Paloma Maquez <maquez.paloma@gmail.com>
2021-08-28 16:21:14 -0300

    nav

--- main.js
+++ main.js
@@ -3,12 +3,25 @@ const navSlide = () => {
     const nav = document.querySelector ('.nav-links');
     const navLinks = document.querySelectorAll ('.nav-links li');
     //Toggle Nav
-    burger.addEventListener('click' , () => {
+    burger.addEventListener('click' , () => 
+    {
         nav.classList.toggle('nav-active')
-    })
+
     //Animtate Links
-    navLinks.forEach((link,index) => {
-        link.style.animation = `navLinksFade 0.5s ease forwards ${index / 7 + 2}s`;
+    navLinks.forEach((link,index) => 
+    {
+            if(link.style.animation)
+            {
+                link.style.animation = ''
+            }else
+            {
+                link.style.animation = `navLinksFade 0.5s ease forwards ${index / 7 + 1.5}s`;
+            }
+        })
     })
+    //Burger Animation
+    burger.classList.toggle ('toggle');
+
+
 }
-navSlide ()
+navSlide ();
