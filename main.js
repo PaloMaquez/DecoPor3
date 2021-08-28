ce4482caf9a1c5a73782f3ee220df271689e1712
Paloma Maquez <maquez.paloma@gmail.com>
2021-08-28 13:58:37 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,14 @@
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector ('.nav-links');
+    const navLinks = document.querySelectorAll ('.nav-links li');
+    //Toggle Nav
+    burger.addEventListener('click' , () => {
+        nav.classList.toggle('nav-active')
+    })
+    //Animtate Links
+    navLinks.forEach((link,index) => {
+        link.style.animation = `navLinksFade 0.5s ease forwards ${index / 7 + 2}s`;
+    })
+}
+navSlide ()
