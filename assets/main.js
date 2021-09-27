20d46589fde8154ff5c1c84ecba47a472768b6c0
Paloma Maquez <maquez.paloma@gmail.com>
2021-09-27 10:28:42 -0300

    deletion js

--- assets/main.js
+++ assets/main.js
@@ -1,27 +1,3 @@
-const navSlide = () => {
-    const burger = document.querySelector('.burger');
-    const nav = document.querySelector ('.nav-links');
-    const navLinks = document.querySelectorAll ('.nav-links li');
-    //Toggle Nav
-    burger.addEventListener('click' , () => 
-    {
-        nav.classList.toggle('nav-active');
 
-    //Animtate Links
-    navLinks.forEach((link,index) => 
-    {
-            if(link.style.animation)
-            {
-                link.style.animation = '';
-            }else
-            {
-                link.style.animation = `navLinksFade 0.5s ease forwards ${index / 7 + 1.5}s`;
-            }
-        })
-        //Burger Animation
-        burger.classList.toggle ('toggle');
-    })
-}
-navSlide ();
 
 
