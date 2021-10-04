6099d7ec3fc71d0bba2875ae7298a036b0c33236
Paloma Maquez <maquez.paloma@gmail.com>
2021-10-03 23:36:18 -0300

    servicios scroll

--- assets/main.js
+++ assets/main.js
@@ -6,5 +6,17 @@ sr.reveal('.wrapper', {
     duration: 3000,
     scale: 0.5
 });
-
-
+sr.reveal ("#left_img");
+sr.reveal('#left_img', {
+    origin : 'right',
+    distance : '50px',
+    duration: 3000,
+    scale: 0.5
+});
+sr.reveal ("#right_img");
+sr.reveal('#right_img', {
+    origin : 'right',
+    distance : '50px',
+    duration: 3000,
+    scale: 0.5
+});
