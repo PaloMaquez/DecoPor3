2d5336ef264898eeccbe4a8fe2fe5e21e7a62967
Paloma Maquez <maquez.paloma@gmail.com>
2021-10-04 01:45:23 -0300

    pissible change

--- assets/main.js
+++ assets/main.js
@@ -8,14 +8,14 @@ sr.reveal('.wrapper', {
 });
 sr.reveal ("#left_img");
 sr.reveal('#left_img', {
-    origin : 'right',
+    origin : 'top',
     distance : '50px',
     duration: 3000,
     scale: 0.5
 });
 sr.reveal ("#right_img");
 sr.reveal('#right_img', {
-    origin : 'right',
+    origin : 'top',
     distance : '50px',
     duration: 3000,
     scale: 0.5
