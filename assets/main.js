74d80655b7b12d314c51bed79dfc104f5ad91053
Paloma Maquez <maquez.paloma@gmail.com>
2021-10-08 17:29:16 -0300

    ajustes

--- assets/main.js
+++ assets/main.js
@@ -3,20 +3,7 @@ sr.reveal (".wrapper");
 sr.reveal('.wrapper', {
     origin : 'bottom',
     distance : '50px',
-    duration: 3000,
+    duration: 2800,
     scale: 0.5
 });
-sr.reveal ("#left_img");
-sr.reveal('#left_img', {
-    origin : 'top',
-    distance : '50px',
-    duration: 3000,
-    scale: 0.5
-});
-sr.reveal ("#right_img");
-sr.reveal('#right_img', {
-    origin : 'top',
-    distance : '50px',
-    duration: 3000,
-    scale: 0.5
-});
+
